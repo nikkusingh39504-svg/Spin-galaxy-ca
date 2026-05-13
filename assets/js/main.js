75829043fc0811256fa31162c64256f8edb3d2f4
nikkusingh39504-svg <nikkusingh39504@gmail.com>
2026-05-13 15:34:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,12 +88,12 @@ if(form){
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', 
                                                 function(){
-    window.location.href = "http://w23hub.com/?utm_campaign=d5sjRjL1yk&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://hyperbliss.site/"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', 
                                                function(){
-    window.location.href = "http://w23hub.com/?utm_campaign=d5sjRjL1yk&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://hyperbliss.site/"; // change to your target page
   });
 })();
